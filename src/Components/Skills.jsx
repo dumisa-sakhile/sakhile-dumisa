79f539aae7d5eeaed531643568de0be064dd8c55
dumisa-sakhile <contact.dumisa@gmail.com>
2024-09-17 23:32:53 +0200

    update tech stack

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -8,56 +8,116 @@ const Skills = () => {
 
       <br />
 
-      <div className="w-[50%] items-center justify-center">
-        <ul className="timeline timeline-vertical">
-          <li>
-            <div className="timeline-start timeline-box">HTML</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-end timeline-box">CSS</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-start timeline-box">JavaScript</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-end timeline-box">TailwindCSS</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-start timeline-box">ReactJS</div>
-          </li>
-          <li>
-            <div className="timeline-start timeline-box">NodeJS</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-end timeline-box">ExpressJS</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-start timeline-box">MongoDB</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-end timeline-box">Supabase</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className="timeline-start timeline-box">Firebase</div>
-          </li>
-        </ul>
+      <div className="w-[40%] flex items-start justify-start flex-wrap gap-6">
+        {/* html */}
+        <a
+          href="https://www.w3.org/html/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/353884/html-5.svg"
+            alt="html"
+            className="w-14"
+          />
+        </a>
+
+        {/* css */}
+        <a
+          href="https://www.w3.org/Style/CSS/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/452185/css-3.svg"
+            alt="css"
+            className="w-14"
+          />
+        </a>
+
+        {/* javascript */}
+        <a
+          href="https://www.javascript.com/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/349419/javascript.svg"
+            alt="javascript"
+            className="w-14"
+          />
+        </a>
+
+        {/* react */}
+        <a
+          href="https://reactjs.org/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/355190/reactjs.svg"
+            alt="react"
+            className="w-14"
+          />
+        </a>
+
+        {/* node */}
+        <a
+          href="https://nodejs.org/en/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/303658/nodejs-1-logo.svg"
+            alt="node"
+            className="w-14"
+          />
+        </a>
+
+        {/* mongodb */}
+        <a
+          href="https://www.mongodb.com/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/331488/mongodb.svg"
+            alt="mongodb"
+            className="w-14"
+          />
+        </a>
+
+        {/* express */}
+        <a
+          href="https://expressjs.com/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/376367/express.svg"
+            alt="express"
+            className="w-14"
+          />
+        </a>
+
+        {/* tailwind */}
+        <a
+          href="https://tailwindcss.com/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/354431/tailwindcss-icon.svg"
+            alt="tailwind"
+            className="w-14"
+          />
+        </a>
+
+        {/* sass */}
+        <a
+          href="https://sass-lang.com/"
+          target="_blank"
+          className="p-4 bg-[#222] rounded-full shadow-lg">
+          <img
+            src="https://www.svgrepo.com/show/374061/sass.svg"
+            alt="sass"
+            className="w-14"
+          />
+        </a>
       </div>
+
       <br />
       <br />
     </>
