d10dbf49536ed3426e5475cdae2365f9e45a3e24
dumisa-sakhile <contact.dumisa@gmail.com>
2025-05-27 02:24:11 +0200

    add google fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="roboto-condensed-light text-sm ">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, architecto corporis eum deleniti voluptate et ex animi dolorem quam accusamus!</p>
     </div>
   );
 }
